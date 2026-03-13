800dd897ac9bf592310a6d378f0371aeb6f21318
Brandon Harrington <brandon.harrington.abq@gmail.com>
2026-03-13 17:47:58 -0500

    Adds the Math ML Library, which translates expressions trees into Math ML.

---
+++ scripts/mathML.js
@@ -0,0 +1,60 @@
+
+function mathMLLib(list, tree, expr, markup, parse) {
+
+	const textContainer = (tag, attributes, txt) => 
+		tree.node(
+			markup.el(tag, attributes), 
+			list.build(tree.leaf(markup.text(txt)))
+		);
+
+	const markupExpr = ev => expr.matchType(ev)(
+		n   => textContainer('mn', list.nil, n), 
+		v   => textContainer('mi', list.nil, v),
+		vop => {
+			if (expr.symbol(vop) === 'Placeholder')
+				return textContainer(
+					'span', 
+					list.build(
+						markup.attr(
+							'class',
+							'placeholder'
+						)
+					),
+					'&nbsp;');
+			else
+				return textContainer('mo', list.nil, expr.symbol(vop))
+		},
+		uop => textContainer('mo', list.nil, expr.symbol(uop)),
+		bop => textContainer('mo', list.nil, expr.symbol(bop))
+	);
+
+	const markupExprTree = tree.foldr(
+		markupExpr,
+		(v, cs) => {
+			const op = v.value;
+
+			if (expr.fixity(op) === expr.infix &&
+				(expr.symbol(op) === '/') ||
+				(expr.symbol(op) === '^'))
+
+				return tree.node(
+					markup.el(expr.symbol(op) === '/' ? 'mfrac' : 'msup', list.nil),
+					cs);
+			else
+				return tree.node(
+					markup.el('mrow', list.nil),
+					expr.matchFixity(v.value)(
+						prefixOp  => list.cons(markupExpr(v), cs),
+						infixOp   => list.cons(list.head(cs), list.cons(markupExpr(v), list.tail(cs))),
+						postfixOp => list.append(markupExpr(v), cs)
+					)
+				);
+		}
+	);
+
+	return Object.freeze({
+		__proto__: null,
+
+		markupExprTree: markupExprTree
+	})
+}
